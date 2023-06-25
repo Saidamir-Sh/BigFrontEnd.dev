caa80dcd97fa00f0b88c303c930f6f6232733cb0
Saidamir-Sh <saidamirpl@gmail.com>
2023-06-25 18:15:15 +0200

    false and true

---
+++ JavaScript Quizzezs/26.true-false.js
@@ -0,0 +1,10 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log([] == 0) // true
+console.log([] == false) // true
+console.log(!![]) // true
+console.log([1] == 1) // true
+console.log(!![1]) // true
+console.log(Boolean([])) // true
+console.log(Boolean(new Boolean([]))) // true
+console.log(Boolean(new Boolean(false))) // true
