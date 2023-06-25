31461d262a6985e5dc57ddd502490e43be097c3b
Saidamir-Sh <saidamirpl@gmail.com>
2023-06-25 16:57:17 +0200

    name for function experssion

---
+++ JavaScript Quizzezs/20.name-for-function-exp.js
@@ -0,0 +1,22 @@
+// This is a JavaScript Quiz from BFE.dev
+
+function a(){
+}
+
+const b = function() {
+  
+}
+
+const c = function d() {
+  console.log(typeof d)
+  d = 'e'
+  console.log(typeof d)
+}
+
+console.log(typeof a) // function
+console.log(typeof b) // function
+console.log(typeof c) // function
+console.log(typeof d) // undefined
+c() 
+// function
+// function
