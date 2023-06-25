056f56dd4676ab6412054eb540933c5a9a4fd5f5
Saidamir-Sh <saidamirpl@gmail.com>
2023-06-25 18:26:07 +0200

    hoisting I

---
+++ JavaScript Quizzezs/27.hoisting-I.js
@@ -0,0 +1,14 @@
+// This is a JavaScript Quiz from BFE.dev
+
+const a = 1
+console.log(a) // 1
+
+var b
+console.log(b) // undefined
+b = 2
+
+console.log(c) // undefined
+var c = 3
+
+console.log(d) // Hoisting does not apply variables declared with let and const - Error
+let d = 2
