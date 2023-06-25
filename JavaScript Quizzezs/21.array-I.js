bca8ec1982c301baf17f0c8b6c31e9b14c017c69
Saidamir-Sh <saidamirpl@gmail.com>
2023-06-25 17:04:32 +0200

    array I

---
+++ JavaScript Quizzezs/21.array-I.js
@@ -0,0 +1,17 @@
+// This is a JavaScript Quiz from BFE.dev
+
+const a = [0]
+console.log(a.length) // 1
+a[3] = 3
+console.log(a.length) // 4
+for (let item of a) {
+  console.log(item) // 0, , , 3
+}
+a.map(item => {console.log(item)}) // 0, 3 
+a.forEach(item => {console.log(item)}) // 0, 3
+console.log(Object.keys(a)) // ['0', '3']
+delete a[3] // return true
+console.log(a.length) // 1
+a[2] = 2
+a.length = 1
+console.log(a[0],a[1],a[2])
