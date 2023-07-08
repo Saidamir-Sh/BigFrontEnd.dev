a06fa88d9b04e7b818d4dfd1cb0f7d0c5d3da83c
Saidamir-Sh <saidamirpl@gmail.com>
2023-07-08 17:47:22 +0200

    hoisting II

---
+++ JavaScript Quizzezs/32.hoisting-III.js
@@ -0,0 +1,36 @@
+// This is a JavaScript Quiz from BFE.dev
+
+var a = 1
+function a() {
+}
+
+console.log(typeof a) // number
+
+var b
+function b() {
+}
+b = 1
+
+console.log(typeof b) // number
+
+function c() {
+}
+var c = 1;
+
+console.log(typeof c) // number
+
+var d = 1;
+
+(function(){
+  d = '2'
+  console.log(typeof d) // string
+  function d() {
+  }
+})()
+
+console.log(typeof d) // number
+
+var e = 1
+const f = function e() {}
+
+console.log(typeof e) // number
