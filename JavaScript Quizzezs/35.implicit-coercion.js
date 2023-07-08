375fed396e5048e3963788f38a82c84360959a23
Saidamir-Sh <saidamirpl@gmail.com>
2023-07-08 18:17:21 +0200

    implicit coersion

---
+++ JavaScript Quizzezs/35.implicit-coercion.js
@@ -0,0 +1,23 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log( [] + {} )
+console.log( + {} ) 
+console.log( + [] )
+console.log( {} + [])
+console.log( ({}) + [])
+console.log( ({}) + [])
+console.log( ({}) + [])
+console.log( {} +  + [])
+console.log( {} +  + [] + {} )
+console.log( {} +  + [] + {}  + [])
+
+// '[object Object]'
+// NaN
+// 0
+// '[object Object]'
+// '[object Object]'
+// '[object Object]'
+// '[object Object]'
+// '[object Object]0'
+// '[object Object]0[object Object]'
+// '[object Object]0[object Object]'
