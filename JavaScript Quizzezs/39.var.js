e33537f8a8e3f4a35f4f44904c2f28502bcc31af
Saidamir-Sh <saidamirpl@gmail.com>
2023-07-09 17:17:45 +0200

    var hoiting

---
+++ JavaScript Quizzezs/39.var.js
@@ -0,0 +1,10 @@
+// This is a JavaScript Quiz from BFE.dev
+
+function foo() {
+    console.log(i) // undefined
+    for (var i = 0; i < 3; i++) {
+      console.log(i) // 1 2 
+    }
+  }
+  
+foo()
