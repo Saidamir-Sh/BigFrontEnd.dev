8203a216c89da90b66d30e02d9595ae326c6c461
Saidamir-Sh <saidamirpl@gmail.com>
2023-07-09 18:02:42 +0200

    hoisting V

---
+++ JavaScript Quizzezs/42.hoisting-V.js
@@ -0,0 +1,44 @@
+// This is a JavaScript Quiz from BFE.dev
+
+(() => {
+    if (!fn) {
+      function fn() {
+        console.log('2')
+      }
+    }
+    fn()
+  })()
+  
+  function fn() {
+    console.log('1')
+  }
+  
+  // another one
+  function fn1() {
+    console.log('3')
+  }
+  
+  (() => {
+    if (!fn1) {
+      function fn1() {
+        console.log('4')
+      }
+    }
+    fn1()
+  })()
+  
+  
+  // another one !
+  (() => {
+    if (false) {
+      function fn3() {
+        console.log('5')
+      }
+    }
+    fn3()
+  })()
+
+//   Answer
+// '2'
+// '4'
+// Errors
