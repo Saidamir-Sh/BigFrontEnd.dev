df146fc47c7b063843434bd748a71b9e0a386d4d
Saidamir-Sh <saidamirpl@gmail.com>
2023-08-06 19:34:31 +0200

    implicit coercion iv

---
+++ JavaScript Quizzezs/46.implicit-coersion-IV.js
@@ -0,0 +1,10 @@
+// This is a JavaScript Quiz from BFE.dev
+
+
+const foo = [0]
+if (foo) {
+  console.log(foo == true) // false - when using '==' both sides convert to numbers: 0 == 1 -> false
+} else {
+  console.log(foo == false)
+}
+
