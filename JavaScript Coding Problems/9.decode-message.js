0a528c53b3c3227ea58caeb13c19eea99a7bc885
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-07 20:06:41 +0100

    decode the message

--- JavaScript Coding Problems/9.decode-message.js
+++ JavaScript Coding Problems/9.decode-message.js
@@ -12,6 +12,24 @@
 // stop when cannot neither move down right or up right. the character on the path is the message
 // for the input above, IROCLED should be returned.
 
-// notes
-
+// notes:
 // if no characters could be collected, return empty string
+/**
+ * @param {string[][]} message
+ * @return {string}
+ */
+function decode(message) {
+    let row = 0, col = 0
+    let result = ''
+    
+    while(message[row] && message[row][col]) {
+        // keep adding characters on the path until we reach end
+        result += message[row][col]
+        // if we still can move diagonally down right then increment the row , else move up right by decrementing row
+        message[row + 1] ? row++ : row--
+        // constantly increment col because we are moving diagonally
+        col++
+    }
+    
+    return result
+  }
