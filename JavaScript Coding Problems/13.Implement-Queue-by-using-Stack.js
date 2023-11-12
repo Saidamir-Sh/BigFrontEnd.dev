e6430fe4a0323c697b7702d36685ac69e1c5f8aa
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-12 12:05:39 +0100

    implement queue

--- JavaScript Coding Problems/13.Implement-Queue-by-using-Stack.js
+++ JavaScript Coding Problems/13.Implement-Queue-by-using-Stack.js
@@ -43,16 +43,34 @@ class Stack {
 
 // you need to complete the following Class
 class Queue {
-    enqueue(element) { 
-      // add new element to the rare
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+  enqueue(element) { 
+    // always add it to the bottom of the stack,
+    // so that first in element always stays on the top of the stack
+    
+    // transfer all elements from stack1 to stack2 as you want to add the new element to the bottom of stack1
+    for(let i=0; i<this.stack1.size(); i++){
+      this.stack2.push(this.stack1.pop());
     }
-    peek() { 
-      // get the head element
-    }
-    size() { 
-      // return count of element
-    }
-    dequeue() {
-      // remove the head element
+    
+    // now add the element to stack1
+    this.stack1.push(element);
+    
+    // transfer back
+    for(let i=0; i<this.stack2.size(); i++){
+      this.stack1.push(this.stack2.pop());
     }
   }
+  peek() { 
+    return this.stack1.peek();
+  }
+  size() { 
+    return this.stack1.size();
+  }
+  dequeue() {
+    return this.stack1.pop();
+  }
+}
