3e482649aef55ef78f8fcf31edd0bfaa3144654c
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-17 23:31:41 +0100

    add more tasks

---
+++ JavaScript Coding Problems/22. implement JSON.parse().js
@@ -0,0 +1,19 @@
+// This is a follow-up on 21. implement JSON.stringify().
+
+// Believe you are already familiar with JSON.parse(), could you implement your own version?
+
+// In case you are not sure about the spec, MDN here might help.
+
+// JSON.parse() support a second parameter reviver, you can ignore that.
+
+// note
+
+// Don't use JSON.parse() in your code here It doesn't help you practicing your skills.
+
+/**
+ * @param {string} str
+ * @return {object | Array | string | number | boolean | null}
+ */
+function parse(str) {
+    // your code here
+  }
---
+++ JavaScript Coding Problems/23. create a sum().js
@@ -0,0 +1,15 @@
+// Create a sum(), which makes following possible
+
+/**
+ * @param {number} num
+ */
+function sum(num) {
+    // your code here
+  }
+  
+
+const sum1 = sum(1)
+sum1(2) == 3 // true
+sum1(3) == 4 // true
+sum(1)(2)(3) == 6 // true
+sum(5)(-1)(2) == 6 // true
